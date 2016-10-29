213afac0aa09f180138c1c9a805131da5569de33
Adam Harris <adam.jensen.harris@gmail.com>
2016-10-29 16:18:25 -0600

    Added basic stat generator.

--- src/main/webapp/src/app/app.component.ts
+++ src/main/webapp/src/app/app.component.ts
@@ -29,15 +29,47 @@ export class AppComponent implements OnInit
 			alignment: [],
 			stats: this.formBuilder.group(
 			{
-				strength:     ['', Validators.required],
-				dexterity:    ['', Validators.required],
-				constitution: ['', Validators.required],
-				intelligence: ['', Validators.required],
-				wisdom:       ['', Validators.required],
-				charisma:     ['', Validators.required],
+				strength:     [0, Validators.required],
+				dexterity:    [0, Validators.required],
+				constitution: [0, Validators.required],
+				intelligence: [0, Validators.required],
+				wisdom:       [0, Validators.required],
+				charisma:     [0, Validators.required],
 			})
 		});
 	}
+
+	randomStat()
+	{
+		let rolls: number[] = [0, 0, 0, 0];
+		for(var i = 0; i < 4; i++)
+		{
+			rolls[i] = Math.floor(Math.random() * 6) + 1;
+		}
+
+		for (var i = 0; i < 4; i++)
+		{
+			let temp: number;
+			if (rolls[i] > rolls[i + 1])
+			{
+				// SWAP the values
+				rolls[i + 1] = [rolls[i], rolls[i] = rolls[i + 1]][0];
+			}
+		}
+
+		return (rolls[0] + rolls[1] + rolls[2]);
+	}
+
+	rollStats()
+	{
+		this.registerForm.value.stats.strength = this.randomStat();
+		this.registerForm.value.stats.dexterity = this.randomStat();
+		this.registerForm.value.stats.consitution = this.randomStat();
+		this.registerForm.value.stats.intelligence = this.randomStat();
+		this.registerForm.value.stats.wisdom = this.randomStat();
+		this.registerForm.value.stats.charisma = this.randomStat();
+	}
+
 
 	save(model: FormGroup)
 	{
